c832b766f9cb685b313b189715bd2d52a54ef265
KevinSmith <flawedsinsecure@gmail.com>
2023-09-27 13:39:25 -0700

    Lab2

---
+++ Lab2partb.py
@@ -0,0 +1,31 @@
+#Step 2
+number1=10
+number2=11
+number3=12
+number4=13
+#Step 3
+add=number1+number2+number3+number4
+mult=number1*number2*number3*number4
+subtract=number4-number3-number2-number1
+division=(number4/number3)
+#Step 4
+print(add, mult, subtract, division)
+#Step 5
+number1=13
+number2=12
+number3=11
+number4=10
+#Step 6
+add=number1+number2+number3+number4
+mult=number1*number2*number3*number4
+subtract=number4-number3-number2-number1
+division=(number4/number3)
+#Step 7
+print(add, mult, subtract, division)
+#Step 8
+add=number1+number2
+mult=number3*number4
+subtract=number1-number2
+division=(number1/number2)
+#Step 9
+print(add, mult, subtract, division)
