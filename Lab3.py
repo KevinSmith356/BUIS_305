4ca2e5db831b83141d8fd8583483030ad24a6be4
KevinSmith <flawedsinsecure@gmail.com>
2023-10-16 13:58:30 -0700

    Lab2

---
+++ Lab3.py
@@ -0,0 +1,35 @@
+# Step 2
+number1=input('Enter number1')
+number2=input('Enter number2')
+print(number1, number2)
+# Step 3
+sum=number1+number2
+print(sum)
+# Step 4
+sum=int(number1)+int(number2)
+print(sum)
+# Step 6
+difference=int(number1)-int(number2)
+multiplication=int(number1)*int(number2)
+division=int(number1)/int(number2)
+print(difference,multiplication,division)
+# Step 7
+number3=input('Enter number3')
+sum=int(number1)+int(number2)+int(number3)
+difference= int(number1)-int(number2)-int(number3)
+multiplication= int(number1)*int(number2)*int(number3)
+division=int(number1)/int(number2)/int(number3)
+# Step 8
+import math
+print(math.pow(9,3))
+# Step 10
+print(math.sqrt(16))
+print(math.cos(8))
+print(math.sin(7))
+print(math.fabs(20))
+print(math.erf(32))
+print(math.expm1(5))
+print(math.factorial(87))
+print(math.radians(44))
+print(math.tanh(100))
+print(math.tan(88))
